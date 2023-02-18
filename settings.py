8c4189a6a7ceb29b66ef3269ec36757c8081df98
Salavei <kano.mori@mail.ru>
2023-02-18 15:46:14 +0300

    refactoring

--- settings.py
+++ settings.py
@@ -8,5 +8,5 @@ env = Env()
 # postgresql+asyncpg://login:password@0.0.0.0:5432/db_name
 REAL_DATABASE_URL = env.str(
     "REAL_DATABASE_URL",
-    default="postgresql+asyncpg://postgres:postgres@0.0.0.0:5432/postgres"
+    default="postgresql+asyncpg://postgres:postgres@0.0.0.0:5433/postgres"
 )  # connect string for the database
